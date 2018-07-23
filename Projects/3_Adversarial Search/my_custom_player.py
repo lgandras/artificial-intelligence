9d60e2c8bd099c9383be691a6cae3a224338aead
Andras Gyomrey <lgandras@gmail.com>
2018-07-23 14:03:12 +0200

    Version with problems with deadlock

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,6 +1,7 @@
 
 from sample_players import DataPlayer
-
+import math, random, time
+from isolation import StopSearch
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -36,11 +37,120 @@ class CustomPlayer(DataPlayer):
           Refer to (and use!) the Isolation.play() function to run games.
         **********************************************************************
         """
-        # TODO: Replace the example implementation below with your own search
-        #       method by combining techniques from lecture
-        #
-        # EXAMPLE: choose a random move without any search--this function MUST
-        #          call self.queue.put(ACTION) at least once before time expires
-        #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+        #print("Starting...")
+        # update context
+        if self.context is not dict:
+            self.context = {
+                'state': state,
+                'action': None,
+                'children': None,
+                'explored': False,
+                'currentBestChild': None,
+                'visits': 0,
+                'reward': 0
+            }
+        else:
+            self.context = self.get_child_node_from_state(state)
+        # search
+        while True:
+            #print("Searching...")
+            node = self.search()
+            #print(node['state'])
+            #print(node['action'])
+            try:
+                self.queue.put(node['action'])
+            except StopSearch:
+                #print("Stop")
+                return
+            #print("Ended search.")
+
+    def get_child_node_from_state(self, state):
+        #print(self.__dict__)
+        for childNode in self.get_children(self.context['currentBestChild']):
+            if state == childNode['state']:
+                #print(childNode)
+                return childNode
+        #print(self.__dict__)
+        raise Exception("Unexpected state to visit: {}".format(state))
+
+    def search(self):
+        if not self.context['explored']:
+            self.visit(self.context)
+        self.context['currentBestChild'] = self.select(self.context, root=True)
+        return self.context['currentBestChild']
+
+    def visit(self, node):
+        if node['state'].terminal_test():
+            reward = self.calculate_reward(node['state'])
+            node['explored'] = True
+        elif node['children'] is None:
+            childNode = self.select(node, root=False)
+            reward = self.simulate(childNode)
+            node['explored'] = all(childNode['explored'] for childNode in node['children'])
+        else:
+            childNode = self.select(node, root=False)
+            reward = self.visit(childNode)
+            node['explored'] = all(childNode['explored'] for childNode in node['children'])
+
+        node['visits'] += 1
+        node['reward'] += reward
+        return reward
+
+    def select(self, node, root=True):
+        bestNode = None
+        bestUcb1 = None
+        for childNode in self.get_children(node):
+            # from the root, we want the best instead of exploring the optimal new path
+            if childNode['explored'] and not root:
+                continue
+            if 0 == childNode['visits']:
+                bestNode = childNode
+                break
+            ucb1 = childNode['reward'] / childNode['visits'] + (0 if root else 1) * math.sqrt(2 * math.log(childNode['visits']) / float(node['visits']))
+            if bestUcb1 is None or bestUcb1 < ucb1:
+                bestNode = childNode
+                bestUcb1 = ucb1
+        return bestNode
+
+    def get_children(self, node):
+        if node['children'] is None:
+            node['children'] = []
+            actions = node['state'].actions()
+            random.shuffle(actions)
+            for action in actions:
+                node['children'].append({
+                    'state': node['state'].result(action),
+                    'action': action,
+                    'children': None,
+                    'explored': False,
+                    'currentBestChild': None,
+                    'visits': 0,
+                    'reward': 0
+                })
+        return node['children']
+
+    def simulate(self, node):
+        state = node['state']
+        while not state.terminal_test():
+            state = state.result(random.choice(state.actions()))
+        return self.calculate_reward(state)
+
+    def calculate_reward(self, state):
+        player = self.context['state'].player()
+        #print(state)
+        val = len(state.liberties(state.locs[player])) - len(state.liberties(state.locs[player ^ 1]))
+        #print("Val={}".format(val))
+        return val
+
+    #@staticmethod
+    #def calculate_reward(state, player):
+    #    return len(state.liberties(state.locs[player]))
+
+"""
+    @staticmethod
+    def calculate_reward(state, player):
+        print(state)
+        val = 1 if 0 < state.utility(player) else 0
+        print("Val={}".format(val))
+        return val
+"""
